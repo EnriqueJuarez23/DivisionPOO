9e124c7d7915f5cf732f0db8da066b9b3560f44e
Enrique Juarez <enriquejuarez.ej7@gmail.com>
2024-05-05 21:37:21 -0600

    Division con modificadores set & get

--- Division.java
+++ Division.java
@@ -1,6 +1,6 @@
 public class Division {
     // nombre de la clase
-    double a,b,r;
+    private double a,b,r;
     // declaracion de variable
     public double mostrarDivision(double a, double b){
     // metodo o comportamiento de la clase
@@ -8,6 +8,29 @@ public class Division {
     // referencia de las variables, realizando la operacion
     return r;
     // retorno de la operacion
+    }
+    public double getA() {
+        return a;
+    }
+    public void setA(double a) {
+        this.a = a;
+    }
+    public double getB() {
+        return b;
+    }
+    public void setB(double b) {
+        this.b = b;
+    }
+    public double getR() {
+        this.r=this.a / this.b;
+        return r;
+    }
+    public void setR(double r) {
+        this.r = r;
+    }
+    @Override
+    public String toString() {
+        return "Division [a=" + a + ", b=" + b + ", r=" + r + "]";
     }
 }
 // cierre del progama con llaves
--- MainDivision.java
+++ MainDivision.java
@@ -14,15 +14,13 @@ public class MainDivision {
         // impresion del mensaje
         double b= opc.nextDouble();
         // declaracion de el tipo de variable
-        double r= a/b;
-        // operacion
         Division div = new Division();
         // metodo constructor del objeto a llamar
-        div.a=a;
-        div.b=b;
-        div.r=r;
+        div.setA(a);
+        div.setB(b);
+        div.getR();
         // almacenamiento de las variables
-        System.out.println("El resultado es:"+ div.mostrarDivision(a, b));
+        System.out.println("El resultado es:"+ div.toString());
         // concatenacion del resultado
     }
 }
